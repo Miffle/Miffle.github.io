c8919393c7692da53c638d6b1c1280dcdce14d97
Roman Mezhuev <miffle1337@mail.ru>
2023-02-27 15:07:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ tg.expand();
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#2cab37";
 tg.MainButton.setText("Купить!").hide();
+let first_light_price = 500;
+let second_light_price = 500;
+let third_light_price = 500;
+let fourth_light_price = 500;
+let fifth_light_price = 500;
+let sixth_light_price = 500;
 let items = [];
 let item1 = 0
 let item2 = 0
@@ -18,6 +24,7 @@ let cost_item3 = 0
 let cost_item4 = 0
 let cost_item5 = 0
 let cost_item6 = 0
+let totalCost = 0
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
 let btn3 = document.getElementById("btn3");
@@ -36,10 +43,13 @@ let lable3 = document.getElementById("count3");
 let lable4 = document.getElementById("count4");
 let lable5 = document.getElementById("count5");
 let lable6 = document.getElementById("count6");
-let clear = document.getElementById("clear_storage")
 
-function MainButtonShow() {
+
+function MainButtonShow(cost) {
+    totalCost += cost;
     if (tg.MainButton.isVisible !== true) {
+
+        tg.MainButton.setText(`Купить! ${totalCost} ₽`)
         tg.MainButton.show();
     }
 }
@@ -49,99 +59,105 @@ function SetText(text, item, cost) {
 
 btn1.addEventListener("click", function () {
     item1 += 1;
-    cost_item1 = 500 * item1;
+    cost_item1 = first_light_price * item1;
     SetText(lable1, item1, cost_item1);
-    MainButtonShow();
+    MainButtonShow(first_light_price);
 });
 
 btn2.addEventListener("click", function () {
     item2 += 1;
-    cost_item2 = 500 * item2;
+    cost_item2 = second_light_price * item2;
     SetText(lable2, item2, cost_item2);
-    MainButtonShow();
+    MainButtonShow(second_light_price);
 
 });
 
 btn3.addEventListener("click", function () {
     item3 += 1;
-    cost_item3 = 500 * item3;
+    cost_item3 = third_light_price * item3;
     SetText(lable3, item3, cost_item3);
-    MainButtonShow();
+    MainButtonShow(third_light_price);
 
 });
 
 btn4.addEventListener("click", function () {
     item4 += 1;
-    cost_item4 = 500 * item4;
+    cost_item4 = fourth_light_price * item4;
     SetText(lable4, item4, cost_item4);
-    MainButtonShow();
+    MainButtonShow(fourth_light_price);
 
 });
 
 btn5.addEventListener("click", function () {
     item5 += 1;
-    cost_item5 = 500 * item5;
+    cost_item5 = fifth_light_price * item5;
     SetText(lable5, item5, cost_item5);
 
-    MainButtonShow();
+    MainButtonShow(fifth_light_price);
 
 });
 
 btn6.addEventListener("click", function () {
     item6 += 1;
-    cost_item6 = 500 * item6;
+    cost_item6 = sixth_light_price * item6;
     SetText(lable6, item6, cost_item6);
-    MainButtonShow();
+    MainButtonShow(sixth_light_price);
 
 });
 
 min_btn1.addEventListener('click', function () {
     if (item1 >= 1) {
         item1 -= 1;
-        cost_item1 = 500 * item1;
+        cost_item1 = first_light_price * item1;
         SetText(lable1, item1, cost_item1);
+        MainButtonShow(first_light_price*(-1));
     }
-    MainButtonShow();
+
 })
 min_btn2.addEventListener('click', function () {
     if (item2 >= 1) {
         item2 -= 1;
-        cost_item2 = 500 * item2;
+        cost_item2 = second_light_price * item2;
         SetText(lable2, item2, cost_item2);
+        MainButtonShow(second_light_price*(-1));
     }
-    MainButtonShow();
+
 })
 min_btn3.addEventListener('click', function () {
     if (item3 >= 1) {
         item3 -= 1;
-        cost_item3 = 500 * item3;
+        cost_item3 = third_light_price * item3;
         SetText(lable3, item3, cost_item3);
+        MainButtonShow(third_light_price*(-1));
     }
-    MainButtonShow();
+
 })
 min_btn4.addEventListener('click', function () {
     if (item4 >= 1) {
         item4 -= 1;
-        cost_item4 = 500 * item4;
+        cost_item4 = fourth_light_price * item4;
         SetText(lable4, item4, cost_item4);
+        MainButtonShow(fourth_light_price*(-1));
     }
-    MainButtonShow();
+
 })
 min_btn5.addEventListener('click', function () {
     if (item5 >= 1) {
         item5 -= 1;
-        cost_item5 = 500 * item5;
+        cost_item5 = fifth_light_price * item5;
         SetText(lable5, item5, cost_item5);
+        MainButtonShow(fifth_light_price*(-1));
     }
-    MainButtonShow();
+
 })
 min_btn6.addEventListener('click', function () {
     if (item6 >= 1) {
         item6 -= 1;
-        cost_item6 = 500 * item6;
+        cost_item6 = sixth_light_price * item6;
         SetText(lable6, item6, cost_item6);
+        MainButtonShow(sixth_light_price*(-1));
     }
-    MainButtonShow();
+
 })
 
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
