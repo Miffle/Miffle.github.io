63937cc749fa916979dbf3dc3af47ff22fc93161
Roman Mezhuev <miffle1337@mail.ru>
2023-02-27 21:19:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ tg.expand();
 
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#2cab37";
-let first_light_price = 500;
-let second_light_price = 500;
-let third_light_price = 500;
-let fourth_light_price = 500;
-let fifth_light_price = 500;
-let sixth_light_price = 500;
+let first_candle_price = 500;
+let second_candle_price = 500;
+let third_candle_price = 500;
+let fourth_candle_price = 500;
+let fifth_candle_price = 500;
+let sixth_candle_price = 500;
 let items = [];
 let item1 = 0
 let item2 = 0
@@ -42,8 +42,13 @@ let lable3 = document.getElementById("count3");
 let lable4 = document.getElementById("count4");
 let lable5 = document.getElementById("count5");
 let lable6 = document.getElementById("count6");
-
-
+let img1 = document.getElementById("img1");
+let img2 = document.getElementById("img2");
+let img3 = document.getElementById("img3");
+let img4 = document.getElementById("img4");
+let img5 = document.getElementById("img5");
+let img6 = document.getElementById("img6");
+let disc1 = document.getElementById("disc1")
 function MainButtonShow(cost) {
     totalCost += cost;
     tg.MainButton.setText(`Купить! ${totalCost} ₽`)
@@ -54,133 +59,139 @@ function MainButtonShow(cost) {
         tg.MainButton.hide();
     }
 }
+img1.addEventListener("click", function (){
+    this.classList.toggle('blur');
+    disc1.classList.toggle('opacity')
 
+
+})
 function SetText(text, item, cost) {
     text.innerText = `${item} шт / Цена ${cost}`;
 }
 
 btn1.addEventListener("click", function () {
     item1 += 1;
-    cost_item1 = first_light_price * item1;
+    cost_item1 = first_candle_price * item1;
     SetText(lable1, item1, cost_item1);
-    MainButtonShow(first_light_price);
+    MainButtonShow(first_candle_price);
 });
 
 btn2.addEventListener("click", function () {
     item2 += 1;
-    cost_item2 = second_light_price * item2;
+    cost_item2 = second_candle_price * item2;
     SetText(lable2, item2, cost_item2);
-    MainButtonShow(second_light_price);
+    MainButtonShow(second_candle_price);
 
 });
 
 btn3.addEventListener("click", function () {
     item3 += 1;
-    cost_item3 = third_light_price * item3;
+    cost_item3 = third_candle_price * item3;
     SetText(lable3, item3, cost_item3);
-    MainButtonShow(third_light_price);
+    MainButtonShow(third_candle_price);
 
 });
 
 btn4.addEventListener("click", function () {
     item4 += 1;
-    cost_item4 = fourth_light_price * item4;
+    cost_item4 = fourth_candle_price * item4;
     SetText(lable4, item4, cost_item4);
-    MainButtonShow(fourth_light_price);
+    MainButtonShow(fourth_candle_price);
 
 });
 
 btn5.addEventListener("click", function () {
     item5 += 1;
-    cost_item5 = fifth_light_price * item5;
+    cost_item5 = fifth_candle_price * item5;
     SetText(lable5, item5, cost_item5);
 
-    MainButtonShow(fifth_light_price);
+    MainButtonShow(fifth_candle_price);
 
 });
 
 btn6.addEventListener("click", function () {
     item6 += 1;
-    cost_item6 = sixth_light_price * item6;
+    cost_item6 = sixth_candle_price * item6;
     SetText(lable6, item6, cost_item6);
-    MainButtonShow(sixth_light_price);
+    MainButtonShow(sixth_candle_price);
 
 });
 
 min_btn1.addEventListener('click', function () {
     if (item1 >= 1) {
         item1 -= 1;
-        cost_item1 = first_light_price * item1;
+        cost_item1 = first_candle_price * item1;
         SetText(lable1, item1, cost_item1);
-        MainButtonShow(first_light_price * (-1));
+        MainButtonShow(first_candle_price * (-1));
     }
 
 })
 min_btn2.addEventListener('click', function () {
     if (item2 >= 1) {
         item2 -= 1;
-        cost_item2 = second_light_price * item2;
+        cost_item2 = second_candle_price * item2;
         SetText(lable2, item2, cost_item2);
-        MainButtonShow(second_light_price * (-1));
+        MainButtonShow(second_candle_price * (-1));
     }
 
 })
 min_btn3.addEventListener('click', function () {
     if (item3 >= 1) {
         item3 -= 1;
-        cost_item3 = third_light_price * item3;
+        cost_item3 = third_candle_price * item3;
         SetText(lable3, item3, cost_item3);
-        MainButtonShow(third_light_price * (-1));
+        MainButtonShow(third_candle_price * (-1));
     }
 
 })
 min_btn4.addEventListener('click', function () {
     if (item4 >= 1) {
         item4 -= 1;
-        cost_item4 = fourth_light_price * item4;
+        cost_item4 = fourth_candle_price * item4;
         SetText(lable4, item4, cost_item4);
-        MainButtonShow(fourth_light_price * (-1));
+        MainButtonShow(fourth_candle_price * (-1));
     }
 
 })
 min_btn5.addEventListener('click', function () {
     if (item5 >= 1) {
         item5 -= 1;
-        cost_item5 = fifth_light_price * item5;
+        cost_item5 = fifth_candle_price * item5;
         SetText(lable5, item5, cost_item5);
-        MainButtonShow(fifth_light_price * (-1));
+        MainButtonShow(fifth_candle_price * (-1));
     }
 
 })
 min_btn6.addEventListener('click', function () {
     if (item6 >= 1) {
         item6 -= 1;
-        cost_item6 = sixth_light_price * item6;
+        cost_item6 = sixth_candle_price * item6;
         SetText(lable6, item6, cost_item6);
-        MainButtonShow(sixth_light_price * (-1));
+        MainButtonShow(sixth_candle_price * (-1));
     }
 
 })
 
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
     if (item1 !== 0) {
-        items.push(`Свеч №1 - ${item1} шт.`)
+        items.push(`Свечей №1 - ${item1} шт.`)
     }
     if (item2 !== 0) {
-        items.push(`Свеч №2 - ${item2} шт.`)
+        items.push(`Свечей №2 - ${item2} шт.`)
     }
     if (item3 !== 0) {
-        items.push(`Свеч №3 - ${item3} шт.`)
+        items.push(`Свечей №3 - ${item3} шт.`)
     }
     if (item4 !== 0) {
-        items.push(`Свеч №4 - ${item4} шт.`)
+        items.push(`Свечей №4 - ${item4} шт.`)
     }
     if (item5 !== 0) {
-        items.push(`Свеч №5 - ${item5} шт.`)
+        items.push(`Свечей №5 - ${item5} шт.`)
     }
     if (item6 !== 0) {
-        items.push(`Свеч №6 - ${item6} шт.`)
+        items.push(`Свечей №6 - ${item6} шт.`)
     }
+
     items.push(`Общая сумма = ${totalCost}`);
-    tg.sendData(items);
+    tg.sendData(items)
 });
