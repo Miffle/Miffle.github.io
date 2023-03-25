fd5279fc2cd0d8a091d54a14c79754bd24167df2
Roman Mezhuev <miffle1337@mail.ru>
2023-03-25 23:41:16 +0300

    Update flowerpot.js

--- flowerpot/flowerpot.js
+++ flowerpot/flowerpot.js
@@ -4,11 +4,13 @@ tg.expand();
 
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#2cab37";
-let first_flowerpot_price = 500;
-let second_flowerpot_price = 500;
-let third_flowerpot_price = 500;
-let fourth_flowerpot_price = 500;
-let fifth_flowerpot_price = 500;
+
+let first_candle_price = 500;
+let second_candle_price = 500;
+let third_candle_price = 500;
+let fourth_candle_price = 500;
+let fifth_candle_price = 500;
+let sixth_candle_price = 500;
 let items = [];
 let item1 = 0
 let item2 = 0
@@ -104,110 +106,111 @@ disc4.addEventListener("click", function () {
 disc5.addEventListener("click", function () {
     blurOpacity(img5, this)
 })
-
 btn1.addEventListener("click", function () {
     item1 += 1;
-    cost_item1 = first_flowerpot_price * item1;
+    cost_item1 = first_candle_price * item1;
     SetText(label1, item1, cost_item1);
-    MainButtonShow(first_flowerpot_price);
+    MainButtonShow(first_candle_price);
 });
 
 btn2.addEventListener("click", function () {
     item2 += 1;
-    cost_item2 = second_flowerpot_price * item2;
+    cost_item2 = second_candle_price * item2;
     SetText(label2, item2, cost_item2);
-    MainButtonShow(second_flowerpot_price);
+    MainButtonShow(second_candle_price);
 
 });
 
 btn3.addEventListener("click", function () {
     item3 += 1;
-    cost_item3 = third_flowerpot_price * item3;
+    cost_item3 = third_candle_price * item3;
     SetText(label3, item3, cost_item3);
-    MainButtonShow(third_flowerpot_price);
+    MainButtonShow(third_candle_price);
 
 });
 
 btn4.addEventListener("click", function () {
     item4 += 1;
-    cost_item4 = fourth_flowerpot_price * item4;
+    cost_item4 = fourth_candle_price * item4;
     SetText(label4, item4, cost_item4);
-    MainButtonShow(fourth_flowerpot_price);
+    MainButtonShow(fourth_candle_price);
 
 });
 
 btn5.addEventListener("click", function () {
     item5 += 1;
-    cost_item5 = fifth_flowerpot_price * item5;
+    cost_item5 = fifth_candle_price * item5;
     SetText(label5, item5, cost_item5);
 
-    MainButtonShow(fifth_flowerpot_price);
+    MainButtonShow(fifth_candle_price);
 
 });
 
 min_btn1.addEventListener('click', function () {
     if (countCheck(item1)) {
         item1 -= 1;
-        cost_item1 = first_flowerpot_price * item1;
+        cost_item1 = first_candle_price * item1;
         SetText(label1, item1, cost_item1);
-        MainButtonShow(first_flowerpot_price * (-1));
+        MainButtonShow(first_candle_price * (-1));
     }
 
 })
 min_btn2.addEventListener('click', function () {
     if (countCheck(item2)) {
         item2 -= 1;
-        cost_item2 = second_flowerpot_price * item2;
+        cost_item2 = second_candle_price * item2;
         SetText(label2, item2, cost_item2);
-        MainButtonShow(second_flowerpot_price * (-1));
+        MainButtonShow(second_candle_price * (-1));
     }
 
 })
 min_btn3.addEventListener('click', function () {
     if (countCheck(item3) === true) {
         item3 -= 1;
-        cost_item3 = third_flowerpot_price * item3;
+        cost_item3 = third_candle_price * item3;
         SetText(label3, item3, cost_item3);
-        MainButtonShow(third_flowerpot_price * (-1));
+        MainButtonShow(third_candle_price * (-1));
     }
 
 })
 min_btn4.addEventListener('click', function () {
     if (countCheck(item4)) {
         item4 -= 1;
-        cost_item4 = fourth_flowerpot_price * item4;
+        cost_item4 = fourth_candle_price * item4;
         SetText(label4, item4, cost_item4);
-        MainButtonShow(fourth_flowerpot_price * (-1));
+        MainButtonShow(fourth_candle_price * (-1));
     }
 
 })
 min_btn5.addEventListener('click', function () {
     if (countCheck(item5)) {
         item5 -= 1;
-        cost_item5 = fifth_flowerpot_price * item5;
+        cost_item5 = fifth_candle_price * item5;
         SetText(label5, item5, cost_item5);
-        MainButtonShow(fifth_flowerpot_price * (-1));
+        MainButtonShow(fifth_candle_price * (-1));
     }
 
 })
 
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
     if (countCheck(item1)) {
-        items.push(`Кашпо №1 - ${item1} шт.`)
+        items.push(`Свечей №1 - ${item1} шт.`)
     }
     if (countCheck(item2)) {
-        items.push(`Кашпо №2 - ${item2} шт.`)
+        items.push(`Свечей №2 - ${item2} шт.`)
     }
     if (countCheck(item3)) {
-        items.push(`Кашпо №3 - ${item3} шт.`)
+        items.push(`Свечей №3 - ${item3} шт.`)
     }
     if (countCheck(item4)) {
-        items.push(`Кашпо №4 - ${item4} шт.`)
+        items.push(`Свечей №4 - ${item4} шт.`)
     }
     if (countCheck(item5)) {
-        items.push(`Кашпо №5 - ${item5} шт.`)
+        items.push(`Свечей №5 - ${item5} шт.`)
     }
 
+
     items.push(`Общая сумма = ${totalCost}`);
+    tg.HapticFeedback()
     tg.sendData(items)
 });
