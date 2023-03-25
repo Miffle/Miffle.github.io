4987301f9b7399dd4128c59c93eef995d56522ff
Roman Mezhuev <miffle1337@mail.ru>
2023-03-26 01:47:29 +0300

    Update flowerpot.js

--- flowerpot.js
+++ flowerpot.js
@@ -1,59 +1,53 @@
-let tg = window.Telegram.WebApp;
-
-tg.expand();
-
-tg.MainButton.textColor = "#FFFFFF";
-tg.MainButton.color = "#2cab37";
 let first_flowerpot_price = 500;
 let second_flowerpot_price = 500;
 let third_flowerpot_price = 500;
 let fourth_flowerpot_price = 500;
 let fifth_flowerpot_price = 500;
-let items = [];
-let item1 = 0
-let item2 = 0
-let item3 = 0
-let item4 = 0
-let item5 = 0
-let cost_item1 = 0
-let cost_item2 = 0
-let cost_item3 = 0
-let cost_item4 = 0
-let cost_item5 = 0
-let totalCost = 0
-let btn1 = document.getElementById("flowerpots_btn1");
-let btn2 = document.getElementById("flowerpots_btn2");
-let btn3 = document.getElementById("flowerpots_btn3");
-let btn4 = document.getElementById("flowerpots_btn4");
-let btn5 = document.getElementById("flowerpots_btn5");
-let min_btn1 = document.getElementById("flowerpots_min_btn1");
-let min_btn2 = document.getElementById("flowerpots_min_btn2");
-let min_btn3 = document.getElementById("flowerpots_min_btn3");
-let min_btn4 = document.getElementById("flowerpots_min_btn4");
-let min_btn5 = document.getElementById("flowerpots_min_btn5");
-let label1 = document.getElementById("flowerpots_count1");
-let label2 = document.getElementById("flowerpots_count2");
-let label3 = document.getElementById("flowerpots_count3");
-let label4 = document.getElementById("flowerpots_count4");
-let label5 = document.getElementById("flowerpots_count5");
-let img1 = document.getElementById("flowerpots_img1");
-let img2 = document.getElementById("flowerpots_img2");
-let img3 = document.getElementById("flowerpots_img3");
-let img4 = document.getElementById("flowerpots_img4");
-let img5 = document.getElementById("flowerpots_img5");
-let disc1 = document.getElementById("flowerpots_disc1")
-let disc2 = document.getElementById("flowerpots_disc2")
-let disc3 = document.getElementById("flowerpots_disc3")
-let disc4 = document.getElementById("flowerpots_disc4")
-let disc5 = document.getElementById("flowerpots_disc5")
+let flowerpot_items = [];
+let flowerpot_item1 = 0
+let flowerpot_item2 = 0
+let flowerpot_item3 = 0
+let flowerpot_item4 = 0
+let flowerpot_item5 = 0
+let flowerpot_cost_item1 = 0
+let flowerpot_cost_item2 = 0
+let flowerpot_cost_item3 = 0
+let flowerpot_cost_item4 = 0
+let flowerpot_cost_item5 = 0
+let flowerpot_totalCost = 0
+let flowerpot_btn1 = document.getElementById("flowerpots_btn1");
+let flowerpot_btn2 = document.getElementById("flowerpots_btn2");
+let flowerpot_btn3 = document.getElementById("flowerpots_btn3");
+let flowerpot_btn4 = document.getElementById("flowerpots_btn4");
+let flowerpot_btn5 = document.getElementById("flowerpots_btn5");
+let flowerpot_min_btn1 = document.getElementById("flowerpots_min_btn1");
+let flowerpot_min_btn2 = document.getElementById("flowerpots_min_btn2");
+let flowerpot_min_btn3 = document.getElementById("flowerpots_min_btn3");
+let flowerpot_min_btn4 = document.getElementById("flowerpots_min_btn4");
+let flowerpot_min_btn5 = document.getElementById("flowerpots_min_btn5");
+let flowerpot_label1 = document.getElementById("flowerpots_count1");
+let flowerpot_label2 = document.getElementById("flowerpots_count2");
+let flowerpot_label3 = document.getElementById("flowerpots_count3");
+let flowerpot_label4 = document.getElementById("flowerpots_count4");
+let flowerpot_label5 = document.getElementById("flowerpots_count5");
+let flowerpot_img1 = document.getElementById("flowerpots_img1");
+let flowerpot_img2 = document.getElementById("flowerpots_img2");
+let flowerpot_img3 = document.getElementById("flowerpots_img3");
+let flowerpot_img4 = document.getElementById("flowerpots_img4");
+let flowerpot_img5 = document.getElementById("flowerpots_img5");
+let flowerpot_disc1 = document.getElementById("flowerpots_disc1")
+let flowerpot_disc2 = document.getElementById("flowerpots_disc2")
+let flowerpot_disc3 = document.getElementById("flowerpots_disc3")
+let flowerpot_disc4 = document.getElementById("flowerpots_disc4")
+let flowerpot_disc5 = document.getElementById("flowerpots_disc5")
 
 function MainButtonShow(cost) {
-    totalCost += cost;
-    tg.MainButton.setText(`Купить! ${totalCost} ₽`)
+    flowerpot_totalCost += cost;
+    tg.MainButton.setText(`Купить! ${flowerpot_totalCost} ₽`)
     if (tg.MainButton.isVisible !== true) {
         tg.MainButton.show();
     }
-    if (totalCost === 0) {
+    if (flowerpot_totalCost === 0) {
         tg.MainButton.hide();
     }
 }
@@ -73,141 +67,141 @@ function countCheck(item) {
     }
 }
 
-img1.addEventListener("click", function () {
-    blurOpacity(img1, disc1)
+flowerpot_img1.addEventListener("click", function () {
+    blurOpacity(flowerpot_img1, flowerpot_disc1)
 })
 
-img2.addEventListener("click", function () {
-    blurOpacity(this, disc2)
+flowerpot_img2.addEventListener("click", function () {
+    blurOpacity(this, flowerpot_disc2)
 })
-img3.addEventListener("click", function () {
-    blurOpacity(this, disc3)
+flowerpot_img3.addEventListener("click", function () {
+    blurOpacity(this, flowerpot_disc3)
 })
-img4.addEventListener("click", function () {
-    blurOpacity(this, disc4)
+flowerpot_img4.addEventListener("click", function () {
+    blurOpacity(this, flowerpot_disc4)
 })
-img5.addEventListener("click", function () {
-    blurOpacity(this, disc5)
+flowerpot_img5.addEventListener("click", function () {
+    blurOpacity(this, flowerpot_disc5)
 })
-disc1.addEventListener("click", function () {
-    blurOpacity(img1, this)
+flowerpot_disc1.addEventListener("click", function () {
+    blurOpacity(flowerpot_img1, this)
 })
-disc2.addEventListener("click", function () {
-    blurOpacity(img2, this)
+flowerpot_disc2.addEventListener("click", function () {
+    blurOpacity(flowerpot_img2, this)
 })
-disc3.addEventListener("click", function () {
-    blurOpacity(img3, this)
+flowerpot_disc3.addEventListener("click", function () {
+    blurOpacity(flowerpot_img3, this)
 })
-disc4.addEventListener("click", function () {
-    blurOpacity(img4, this)
+flowerpot_disc4.addEventListener("click", function () {
+    blurOpacity(flowerpot_img4, this)
 })
-disc5.addEventListener("click", function () {
-    blurOpacity(img5, this)
+flowerpot_disc5.addEventListener("click", function () {
+    blurOpacity(flowerpot_img5, this)
 })
 
-btn1.addEventListener("click", function () {
-    item1 += 1;
-    cost_item1 = first_flowerpot_price * item1;
-    SetText(label1, item1, cost_item1);
+flowerpot_btn1.addEventListener("click", function () {
+    flowerpot_item1 += 1;
+    flowerpot_cost_item1 = first_flowerpot_price * flowerpot_item1;
+    SetText(flowerpot_label1, flowerpot_item1, flowerpot_cost_item1);
     MainButtonShow(first_flowerpot_price);
 });
 
-btn2.addEventListener("click", function () {
-    item2 += 1;
-    cost_item2 = second_flowerpot_price * item2;
-    SetText(label2, item2, cost_item2);
+flowerpot_btn2.addEventListener("click", function () {
+    flowerpot_item2 += 1;
+    flowerpot_cost_item2 = second_flowerpot_price * flowerpot_item2;
+    SetText(flowerpot_label2, flowerpot_item2, flowerpot_cost_item2);
     MainButtonShow(second_flowerpot_price);
 
 });
 
-btn3.addEventListener("click", function () {
-    item3 += 1;
-    cost_item3 = third_flowerpot_price * item3;
-    SetText(label3, item3, cost_item3);
+flowerpot_btn3.addEventListener("click", function () {
+    flowerpot_item3 += 1;
+    flowerpot_cost_item3 = third_flowerpot_price * flowerpot_item3;
+    SetText(flowerpot_label3, flowerpot_item3, flowerpot_cost_item3);
     MainButtonShow(third_flowerpot_price);
 
 });
 
-btn4.addEventListener("click", function () {
-    item4 += 1;
-    cost_item4 = fourth_flowerpot_price * item4;
-    SetText(label4, item4, cost_item4);
+flowerpot_btn4.addEventListener("click", function () {
+    flowerpot_item4 += 1;
+    flowerpot_cost_item4 = fourth_flowerpot_price * flowerpot_item4;
+    SetText(flowerpot_label4, flowerpot_item4, flowerpot_cost_item4);
     MainButtonShow(fourth_flowerpot_price);
 
 });
 
-btn5.addEventListener("click", function () {
-    item5 += 1;
-    cost_item5 = fifth_flowerpot_price * item5;
-    SetText(label5, item5, cost_item5);
+flowerpot_btn5.addEventListener("click", function () {
+    flowerpot_item5 += 1;
+    flowerpot_cost_item5 = fifth_flowerpot_price * flowerpot_item5;
+    SetText(flowerpot_label5, flowerpot_item5, flowerpot_cost_item5);
 
     MainButtonShow(fifth_flowerpot_price);
 
 });
 
-min_btn1.addEventListener('click', function () {
-    if (countCheck(item1)) {
-        item1 -= 1;
-        cost_item1 = first_flowerpot_price * item1;
-        SetText(label1, item1, cost_item1);
+flowerpot_min_btn1.addEventListener('click', function () {
+    if (countCheck(flowerpot_item1)) {
+        flowerpot_item1 -= 1;
+        flowerpot_cost_item1 = first_flowerpot_price * flowerpot_item1;
+        SetText(flowerpot_label1, flowerpot_item1, flowerpot_cost_item1);
         MainButtonShow(first_flowerpot_price * (-1));
     }
 
 })
-min_btn2.addEventListener('click', function () {
-    if (countCheck(item2)) {
-        item2 -= 1;
-        cost_item2 = second_flowerpot_price * item2;
-        SetText(label2, item2, cost_item2);
+flowerpot_min_btn2.addEventListener('click', function () {
+    if (countCheck(flowerpot_item2)) {
+        flowerpot_item2 -= 1;
+        flowerpot_cost_item2 = second_flowerpot_price * flowerpot_item2;
+        SetText(flowerpot_label2, flowerpot_item2, flowerpot_cost_item2);
         MainButtonShow(second_flowerpot_price * (-1));
     }
 
 })
-min_btn3.addEventListener('click', function () {
-    if (countCheck(item3) === true) {
-        item3 -= 1;
-        cost_item3 = third_flowerpot_price * item3;
-        SetText(label3, item3, cost_item3);
+flowerpot_min_btn3.addEventListener('click', function () {
+    if (countCheck(flowerpot_item3) === true) {
+        flowerpot_item3 -= 1;
+        flowerpot_cost_item3 = third_flowerpot_price * flowerpot_item3;
+        SetText(flowerpot_label3, flowerpot_item3, flowerpot_cost_item3);
         MainButtonShow(third_flowerpot_price * (-1));
     }
 
 })
-min_btn4.addEventListener('click', function () {
-    if (countCheck(item4)) {
-        item4 -= 1;
-        cost_item4 = fourth_flowerpot_price * item4;
-        SetText(label4, item4, cost_item4);
+flowerpot_min_btn4.addEventListener('click', function () {
+    if (countCheck(flowerpot_item4)) {
+        flowerpot_item4 -= 1;
+        flowerpot_cost_item4 = fourth_flowerpot_price * flowerpot_item4;
+        SetText(flowerpot_label4, flowerpot_item4, flowerpot_cost_item4);
         MainButtonShow(fourth_flowerpot_price * (-1));
     }
 
 })
-min_btn5.addEventListener('click', function () {
-    if (countCheck(item5)) {
-        item5 -= 1;
-        cost_item5 = fifth_flowerpot_price * item5;
-        SetText(label5, item5, cost_item5);
+flowerpot_min_btn5.addEventListener('click', function () {
+    if (countCheck(flowerpot_item5)) {
+        flowerpot_item5 -= 1;
+        flowerpot_cost_item5 = fifth_flowerpot_price * flowerpot_item5;
+        SetText(flowerpot_label5, flowerpot_item5, flowerpot_cost_item5);
         MainButtonShow(fifth_flowerpot_price * (-1));
     }
 
 })
 
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
-    if (countCheck(item1)) {
-        items.push(`Кашпо №1 - ${item1} шт.`)
+    if (countCheck(flowerpot_item1)) {
+        flowerpot_items.push(`Кашпо №1 - ${flowerpot_item1} шт.`)
     }
-    if (countCheck(item2)) {
-        items.push(`Кашпо №2 - ${item2} шт.`)
+    if (countCheck(flowerpot_item2)) {
+        flowerpot_items.push(`Кашпо №2 - ${flowerpot_item2} шт.`)
     }
-    if (countCheck(item3)) {
-        items.push(`Кашпо №3 - ${item3} шт.`)
+    if (countCheck(flowerpot_item3)) {
+        flowerpot_items.push(`Кашпо №3 - ${flowerpot_item3} шт.`)
     }
-    if (countCheck(item4)) {
-        items.push(`Кашпо №4 - ${item4} шт.`)
+    if (countCheck(flowerpot_item4)) {
+        flowerpot_items.push(`Кашпо №4 - ${flowerpot_item4} шт.`)
     }
-    if (countCheck(item5)) {
-        items.push(`Кашпо №5 - ${item5} шт.`)
+    if (countCheck(flowerpot_item5)) {
+        flowerpot_items.push(`Кашпо №5 - ${flowerpot_item5} шт.`)
     }
 
-    items.push(`Общая сумма = ${totalCost}`);
-    tg.sendData(items)
+    flowerpot_items.push(`Общая сумма = ${flowerpot_totalCost}`);
+    tg.sendData(flowerpot_items)
 });
