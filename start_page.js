fab9dcd9414bc2dce97abdff10fb5d184aacb16b
Roman Mezhuev <miffle1337@mail.ru>
2023-03-25 23:30:23 +0300

    Update start_page.js

--- start_page.js
+++ start_page.js
@@ -1,9 +1,12 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
 let candles = document.getElementById("candles")
 let flowerpot = document.getElementById("flowerpot")
 
 
 candles.addEventListener("click", function () {
-    window.location.href = 'candles/candles_page.html';
+    tg.openLink('candles/candles_page.html')
 })
 flowerpot.addEventListener("click", function () {
     window.location.href = 'flowerpot/flowerpot.html';
