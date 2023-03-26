a2e6b0a1b29ee86a77cfd2c00a2077bd7fe6768b
Miffle <miffle1337@mail.ru>
2023-03-27 01:01:45 +0300

    try

--- candles.js
+++ candles.js
@@ -1,152 +1,49 @@
-let candle_prices = [500, 500, 500, 500, 500]
+let candle_price = [500, 500, 500, 500, 500]
 let candle_item = [0,0,0,0,0]
 let candle_cost_item = [0,0,0,0,0]
 let candle_totalCost = 0
-let candle_plus_btn = [document.getElementById("candles_btn1"),
-    document.getElementById("candles_btn2"),
-    document.getElementById("candles_btn3"),
-    document.getElementById("candles_btn4"),
-    document.getElementById("candles_btn5")]
-let candle_min_btn = [document.getElementById("candles_min_btn1"),
-    document.getElementById("candles_min_btn2"),
-    document.getElementById("candles_min_btn3"),
-    document.getElementById("candles_min_btn4"),
-    document.getElementById("candles_min_btn5")]
-let candle_label = [document.getElementById("candles_count1"),
-    document.getElementById("candles_count2"),
-    document.getElementById("candles_count3"),
-    document.getElementById("candles_count4"),
-    document.getElementById("candles_count5")]
-let candle_img = [
-    document.getElementById("candles_img1"),
-    document.getElementById("candles_img2"),
-    document.getElementById("candles_img3"),
-    document.getElementById("candles_img4"),
-    document.getElementById("candles_img5")]
-let candle_disc = [
-    document.getElementById("candles_disc1"),
-    document.getElementById("candles_disc2"),
-    document.getElementById("candles_disc3"),
-    document.getElementById("candles_disc4"),
-    document.getElementById("candles_disc5")]
+const candle_plus_btn_id = ["candles_btn1", "candles_btn2", "candles_btn3", "candles_btn4", "candles_btn5"]
+const candle_min_btn_id = ["candles_min_btn1", "candles_min_btn2", "candles_min_btn3", "candles_min_btn4", "candles_min_btn5"]
+const candle_label_id = ["candles_count1", "candles_count2", "candles_count3", "candles_count4", "candles_count5"]
+const candle_img_id = ["candles_img1", "candles_img2", "candles_img3", "candles_img4", "candles_img5"]
+const candle_disc_id = ["candles_disc1", "candles_disc2", "candles_disc3", "candles_disc4", "candles_disc5"]
+const candle_count = candle_plus_btn_id.length
+let candle_img = []
+let candle_disc = []
+let candle_label = []
+let candle_plus_btn = []
+let candle_min_btn = []
 
-candle_img[0].addEventListener("click", function () {
-    blurOpacity(this, candle_disc[0])
-})
+for (let i = 0; i < candle_count; i++) {
+    candle_label.push(document.getElementById(candle_label_id[i]))
+    candle_plus_btn.push(document.getElementById(candle_plus_btn_id[i]))
+    candle_min_btn.push(document.getElementById(candle_min_btn_id[i]))
+    candle_img.push(document.getElementById(candle_img_id[i]))
+    candle_disc.push(document.getElementById(candle_disc_id[i]))
+}
+for (let j = 0; j < candle_count; j++){
+    candle_plus_btn[j].addEventListener("click", function () {
+        candle_item[j] += 1;
+        candle_cost_item[j] = candle_price[j] * candle_item[j];
+        SetText(candle_label[j], candle_item[j], candle_cost_item[j]);
+        MainButtonShow(candle_price[j]);
+    });
 
-candle_img[1].addEventListener("click", function () {
-    blurOpacity(this, candle_disc[1])
-})
-candle_img[2].addEventListener("click", function () {
-    blurOpacity(this, candle_disc[2])
-})
-candle_img[3].addEventListener("click", function () {
-    blurOpacity(this, candle_disc[3])
-})
-candle_img[4].addEventListener("click", function () {
-    blurOpacity(this, candle_disc[4])
-})
-candle_disc[0].addEventListener("click", function () {
-    blurOpacity(candle_img[0], this)
-})
-candle_disc[1].addEventListener("click", function () {
-    blurOpacity(candle_img[1], this)
-})
-candle_disc[2].addEventListener("click", function () {
-    blurOpacity(candle_img[2], this)
-})
-candle_disc[3].addEventListener("click", function () {
-    blurOpacity(candle_img[3], this)
-})
-candle_disc[4].addEventListener("click", function () {
-    blurOpacity(candle_img[4], this)
-})
-
-candle_plus_btn[0].addEventListener("click", function () {
-    candle_item[0] += 1;
-    candle_cost_item[0] = candle_prices[0] * candle_item[0];
-    SetText(candle_label[0], candle_item[0], candle_cost_item[0]);
-    MainButtonShow(candle_prices[0], "candle");
-});
-
-candle_plus_btn[1].addEventListener("click", function () {
-    candle_item[1] += 1;
-    candle_cost_item[1] = candle_prices[1] * candle_item[1];
-    SetText(candle_label[1], candle_item[1], candle_cost_item[1]);
-    MainButtonShow(candle_prices[1], "candle");
-
-});
-
-candle_plus_btn[2].addEventListener("click", function () {
-    candle_item[2] += 1;
-    candle_cost_item[2] = candle_prices[2] * candle_item[2];
-    SetText(candle_label[2], candle_item[2], candle_cost_item[2]);
-    MainButtonShow(candle_prices[2], "candle");
-
-});
-
-candle_plus_btn[3].addEventListener("click", function () {
-    candle_item[3] += 1;
-    candle_cost_item[3] = candle_prices[3] * candle_item[3];
-    SetText(candle_label[3], candle_item[3], candle_cost_item[3]);
-    MainButtonShow(candle_prices[3], "candle");
-
-});
-
-candle_plus_btn[4].addEventListener("click", function () {
-    candle_item[4] += 1;
-    candle_cost_item[4] = candle_prices[4] * candle_item[4];
-    SetText(candle_label[4], candle_item[4], candle_cost_item[4]);
-
-    MainButtonShow(candle_prices[4], "candle");
-
-});
-
-candle_min_btn[0].addEventListener('click', function () {
-    if (countCheck(candle_item[0])) {
-        candle_item[0] -= 1;
-        candle_cost_item[0] = candle_prices[0] * candle_item[0];
-        SetText(candle_label[0], candle_item[0], candle_cost_item[0]);
-        MainButtonShow(candle_prices[0] * (-1), "candle");
-    }
-
-})
-candle_min_btn[1].addEventListener('click', function () {
-    if (countCheck(candle_item[1])) {
-        candle_item[1] -= 1;
-        candle_cost_item[1] = candle_prices[1] * candle_item[1];
-        SetText(candle_label[1], candle_item[1], candle_cost_item[1]);
-        MainButtonShow(candle_prices[1] * (-1), "candle");
-    }
-
-})
-candle_min_btn[2].addEventListener('click', function () {
-    if (countCheck(candle_item[2]) === true) {
-        candle_item[2] -= 1;
-        candle_cost_item[2] = candle_prices[2] * candle_item[2];
-        SetText(candle_label[2], candle_item[2], candle_cost_item[2]);
-        MainButtonShow(candle_prices[2] * (-1), "candle");
-    }
-
-})
-candle_min_btn[3].addEventListener('click', function () {
-    if (countCheck(candle_item[3])) {
-        candle_item[3] -= 1;
-        candle_cost_item[3] = candle_prices[3] * candle_item[3];
-        SetText(candle_label[3], candle_item[3], candle_cost_item[3]);
-        MainButtonShow(candle_prices[3] * (-1), "candle");
-    }
-
-})
-candle_min_btn[4].addEventListener('click', function () {
-    if (countCheck(candle_item[4])) {
-        candle_item[4] -= 1;
-        candle_cost_item[4] = candle_prices[4] * candle_item[4];
-        SetText(candle_label[4], candle_item[4], candle_cost_item[4]);
-        MainButtonShow(candle_prices[4] * (-1), "candle");
-    }
-
-})
+    candle_min_btn[j].addEventListener('click', function () {
+        if (countCheck(candle_item[j])) {
+            candle_item[j] -= 1;
+            candle_cost_item[j] = candle_price[j] * candle_item[j];
+            SetText(candle_label[j], candle_item[j], candle_cost_item[j]);
+            MainButtonShow(candle_price[j] * (-1));
+        }
+    });
+    candle_img[j].addEventListener('click', function (){
+        blurOpacity(this, candle_disc[j])
+    })
+    candle_disc[j].addEventListener("click", function () {
+        blurOpacity(candle_img[j], this)
+    })
+}
 function MainButtonShow(cost){
     candle_totalCost += cost;
     tg.MainButton.setText(`Купить! ${candle_totalCost} ₽`)
--- main_button_action.js
+++ main_button_action.js
@@ -4,46 +4,36 @@ tg.expand();
 
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#2cab37";
+
 function countCheck(item) {
     if (item > 0) {
         return true
     }
 }
+
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
     let totalCost = candle_totalCost + flowerpot_totalCost;
     let all_items = [];
+
+    const candleNames = ["Свечей 'Грейпфрут'", "Свечей 'Лето на виноградной плантации'", "Свечей 'Уд & Ваниль'", "Свечей 'Свежесть манго'", "Свечей 'Космическая ностальгия'"];
+
+    const flowerpotColors = ["зеленого", "розового", "фиолетового", "черного", "желтого"];
+
+    const candleQuantities = candle_item.filter(countCheck);
+    const flowerpotQuantities = flowerpot_item.filter(countCheck);
+
+    candleQuantities.forEach((quantity, index) => {
+        all_items.push(`${candleNames[index]} - ${quantity} шт.`);
+    });
+
+    flowerpotQuantities.forEach((quantity, index) => {
+        all_items.push(`Кашпо ${flowerpotColors[index]} цвета - ${quantity} шт.`);
+    });
+
     if (totalCost !== 0) {
-        if (countCheck(candle_item[0])) {
-            all_items.push(`Свечей "Грейпфрут" - ${candle_item[0]} шт.`)
-        }
-        if (countCheck(candle_item[1])) {
-            all_items.push(`Свечей "Лето на виноградной плантации" - ${candle_item[1]} шт.`)
-        }
-        if (countCheck(candle_item[2])) {
-            all_items.push(`Свечей "Уд & Ваниль" - ${candle_item[2]} шт.`)
-        }
-        if (countCheck(candle_item[3])) {
-            all_items.push(`Свечей "Свежесть манго" - ${candle_item[3]} шт.`)
-        }
-        if (countCheck(candle_item[4])) {
-            all_items.push(`Свечей "Космическая ностальгия" - ${candle_item[4]} шт.`)
-        }
-        if (countCheck(flowerpot_item[0])) {
-            all_items.push(`Кашпо зеленого цвета - ${flowerpot_item[0]} шт.`)
-        }
-        if (countCheck(flowerpot_item[1])) {
-            all_items.push(`Кашпо розового цвета - ${flowerpot_item[1]} шт.`)
-        }
-        if (countCheck(flowerpot_item[2])) {
-            all_items.push(`Кашп фиолетового цвета" - ${flowerpot_item[2]} шт.`)
-        }
-        if (countCheck(flowerpot_item[3])) {
-            all_items.push(`Кашпо черного цвета - ${flowerpot_item[3]} шт.`)
-        }
-        if (countCheck(flowerpot_item[4])) {
-            all_items.push(`Кашпо желтого цвета - ${flowerpot_item[4]} шт.`)
-        }
-        all_items.push(`Общая сумма = ${totalCost}`)
+        all_items.push(`Общая сумма = ${totalCost}`);
         tg.sendData(all_items)
-    } else {tg.showAlert("Корзина пуста. Чтобы что-то купить - надо что-то выбрать")}
+    } else {
+        tg.showAlert("Корзина пуста. Чтобы что-то купить - надо что-то выбрать")
+    }
 });
