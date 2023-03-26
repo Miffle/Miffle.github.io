0de2656b77d9d71bdb9a5750f8c5f34ce69d9da1
Roman Mezhuev <miffle1337@mail.ru>
2023-03-26 15:33:02 +0300

    Update main_button_action.js

--- main_button_action.js
+++ main_button_action.js
@@ -10,43 +10,42 @@ function countCheck(item) {
     }
 }
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
+    let totalCost = candle_totalCost + flowerpot_totalCost;
+    let all_items = [];
     if (candle_totalCost !== 0) {
         if (countCheck(candle_item[0])) {
-            candles_items.push(`Свечей №1 - ${candle_item[0]} шт.`)
+            all_items.push(`Свечей №1 - ${candle_item[0]} шт.`)
         }
         if (countCheck(candle_item[1])) {
-            candles_items.push(`Свечей №2 - ${candle_item[1]} шт.`)
+            all_items.push(`Свечей №2 - ${candle_item[1]} шт.`)
         }
         if (countCheck(candle_item[2])) {
-            candles_items.push(`Свечей №3 - ${candle_item[2]} шт.`)
+            all_items.push(`Свечей №3 - ${candle_item[2]} шт.`)
         }
         if (countCheck(candle_item[3])) {
-            candles_items.push(`Свечей №4 - ${candle_item[3]} шт.`)
+            all_items.push(`Свечей №4 - ${candle_item[3]} шт.`)
         }
         if (countCheck(candle_item[4])) {
-            candles_items.push(`Свечей №5 - ${candle_item[4]} шт.`)
+            all_items.push(`Свечей №5 - ${candle_item[4]} шт.`)
         }
-        candles_items.push(`Общая сумма = ${candle_totalCost}`)
-        candles_items.push(candle_item[0], candle_item[1], candle_item[2], candle_item[3], candle_item[4])
-        tg.sendData(candles_items)
-    } else {
         if (countCheck(flowerpot_item[0])) {
-            flowerpot_items.push(`Кашпо №1 - ${flowerpot_item[0]} шт.`)
+            all_items.push(`Кашпо №1 - ${flowerpot_item[0]} шт.`)
         }
         if (countCheck(flowerpot_item[1])) {
-            flowerpot_items.push(`Кашпо №2 - ${flowerpot_item[1]} шт.`)
+            all_items.push(`Кашпо №2 - ${flowerpot_item[1]} шт.`)
         }
         if (countCheck(flowerpot_item[2])) {
-            flowerpot_items.push(`Кашпо №3 - ${flowerpot_item[2]} шт.`)
+            all_items.push(`Кашпо №3 - ${flowerpot_item[2]} шт.`)
         }
         if (countCheck(flowerpot_item[3])) {
-            flowerpot_items.push(`Кашпо №4 - ${flowerpot_item[3]} шт.`)
+            all_items.push(`Кашпо №4 - ${flowerpot_item[3]} шт.`)
         }
         if (countCheck(flowerpot_item[4])) {
             flowerpot_items.push(`Кашпо №5 - ${flowerpot_item[4]} шт.`)
         }
-        flowerpot_items.push(`Общая сумма = ${flowerpot_totalCost}`);
-        tg.sendData(flowerpot_items)
+        all_items.push(`Общая сумма = ${totalCost}`)
+        all_items.push(candle_item[0], candle_item[1], candle_item[2], candle_item[3], candle_item[4])
+        tg.sendData(all_items)
     }
 
 });
