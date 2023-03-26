83ce80794f2ea562a7628754c4ec2b7d2293eb5b
Roman Mezhuev <miffle1337@mail.ru>
2023-03-26 15:17:02 +0300

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,7 @@
+function SetText(text, item, cost) {
+    text.innerText = `${item} шт / ${cost}`;
+}
+function blurOpacity(img, disc) {
+    img.classList.toggle('blur');
+    disc.classList.toggle('opacity')
+}
