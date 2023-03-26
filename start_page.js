2683e4af5666c97dd50edba373b159cc5efecff0
Miffle <miffle1337@mail.ru>
2023-03-26 15:54:32 +0300

    try

--- start_page.js
+++ start_page.js
@@ -3,7 +3,7 @@ let flowerpot = document.getElementById("flowerpot")
 let flowerpots_div = document.getElementById("flowerpots")
 let candles_div = document.getElementById("candles")
 let start_page = document.getElementById('start_page')
-
+let header = document.getElementById("header")
 candles.addEventListener("click", function () {
     start_page.hidden = true
     candles_div.hidden = false
@@ -12,4 +12,10 @@ candles.addEventListener("click", function () {
 flowerpot.addEventListener("click", function () {
     start_page.hidden = true
     flowerpots_div.hidden = false
+})
+
+header.addEventListener("click", function (){
+    start_page.hidden = false;
+    candles_div.hidden = true;
+    flowerpots_div.hidden = true;
 })
