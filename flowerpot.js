aa5fa533e1ea9b7a5503247fb0e3f32f4179c64f
Miffle <miffle1337@mail.ru>
2023-03-28 18:30:43 +0300

    try

--- flowerpot.js
+++ flowerpot.js
@@ -3,16 +3,15 @@ let [flowerpot_price, flowerpot_item, flowerpot_cost_item] = [
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0]]
 const [flowerpot_plus_btn_id, flowerpot_min_btn_id, flowerpot_label_id] = [
-    ["flowerpots_btn1", "flowerpots_btn2", "flowerpots_btn3", "flowerpots_btn4", "flowerpots_btn5", "tray_btn1"],
-    ["flowerpots_min_btn1", "flowerpots_min_btn2", "flowerpots_min_btn3", "flowerpots_min_btn4", "flowerpots_min_btn5", "tray_min_btn1"],
-    ["flowerpots_count1", "flowerpots_count2", "flowerpots_count3", "flowerpots_count4", "flowerpots_count5", "tray_count1"]]
+    ["flowerpots_btn1", "flowerpots_btn2", "flowerpots_btn3", "flowerpots_btn4", "flowerpots_btn5", "tray_btn1", "tray_btn2", "tray_btn3", "tray_btn4", "tray_btn5"],
+    ["flowerpots_min_btn1", "flowerpots_min_btn2", "flowerpots_min_btn3", "flowerpots_min_btn4", "flowerpots_min_btn5", "tray_min_btn1", "tray_min_btn2", "tray_min_btn3", "tray_min_btn4", "tray_min_btn5"],
+    ["flowerpots_count1", "flowerpots_count2", "flowerpots_count3", "flowerpots_count4", "flowerpots_count5", "tray_count1", "tray_count2", "tray_count3", "tray_count4", "tray_count5"]]
 const [flowerpot_label, flowerpot_plus_btn, flowerpot_min_btn] = [
     flowerpot_label_id.map(id => document.getElementById(id)),
     flowerpot_plus_btn_id.map(id => document.getElementById(id)),
     flowerpot_min_btn_id.map(id => document.getElementById(id))]
 const flowerpot_count = flowerpot_label_id.length
 let flowerpot_totalCost = 0
-let button = document.getElementById("checking")
 for (let j = 0; j < flowerpot_count; j++) {
     flowerpot_plus_btn[j].addEventListener("click", function () {
         flowerpot_item[j] += 1;
