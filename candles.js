f13cb693049eebafef6eb62a443df314880116d9
Miffle <miffle1337@mail.ru>
2023-03-28 16:33:47 +0300

    try

--- candles.js
+++ candles.js
@@ -1,27 +1,23 @@
 let candle_price = [500, 500, 500, 500, 500]
-let candle_item = [0,0,0,0,0]
-let candle_cost_item = [0,0,0,0,0]
+let candle_item = [0, 0, 0, 0, 0]
+let candle_cost_item = [0, 0, 0, 0, 0]
 let candle_totalCost = 0
-const candle_plus_btn_id = ["candles_btn1", "candles_btn2", "candles_btn3", "candles_btn4", "candles_btn5"]
-const candle_min_btn_id = ["candles_min_btn1", "candles_min_btn2", "candles_min_btn3", "candles_min_btn4", "candles_min_btn5"]
-const candle_label_id = ["candles_count1", "candles_count2", "candles_count3", "candles_count4", "candles_count5"]
-const candle_img_id = ["candles_img1", "candles_img2", "candles_img3", "candles_img4", "candles_img5"]
-const candle_disc_id = ["candles_disc1", "candles_disc2", "candles_disc3", "candles_disc4", "candles_disc5"]
+const [candle_plus_btn_id, candle_min_btn_id, candle_label_id, candle_img_id, candle_disc_id] = [
+    ["candles_btn1", "candles_btn2", "candles_btn3", "candles_btn4", "candles_btn5"],
+    ["candles_min_btn1", "candles_min_btn2", "candles_min_btn3", "candles_min_btn4", "candles_min_btn5"],
+    ["candles_count1", "candles_count2", "candles_count3", "candles_count4", "candles_count5"],
+    ["candles_img1", "candles_img2", "candles_img3", "candles_img4", "candles_img5"],
+    ["candles_disc1", "candles_disc2", "candles_disc3", "candles_disc4", "candles_disc5"]]
 const candle_count = candle_plus_btn_id.length
-let candle_img = []
-let candle_disc = []
-let candle_label = []
-let candle_plus_btn = []
-let candle_min_btn = []
+let [candle_img, candle_disc, candle_label, candle_plus_btn, candle_min_btn] = [
+    candle_img_id.map(id => document.getElementById(id)),
+    candle_disc_id.map(id => document.getElementById(id)),
+    candle_label_id.map(id => document.getElementById(id)),
+    candle_plus_btn_id.map(id => document.getElementById(id)),
+    candle_min_btn_id.map(id => document.getElementById(id))
+]
 
-for (let i = 0; i < candle_count; i++) {
-    candle_label.push(document.getElementById(candle_label_id[i]))
-    candle_plus_btn.push(document.getElementById(candle_plus_btn_id[i]))
-    candle_min_btn.push(document.getElementById(candle_min_btn_id[i]))
-    candle_img.push(document.getElementById(candle_img_id[i]))
-    candle_disc.push(document.getElementById(candle_disc_id[i]))
-}
-for (let j = 0; j < candle_count; j++){
+for (let j = 0; j < candle_count; j++) {
     candle_plus_btn[j].addEventListener("click", function () {
         candle_item[j] += 1;
         candle_cost_item[j] = candle_price[j] * candle_item[j];
@@ -37,14 +33,15 @@ for (let j = 0; j < candle_count; j++){
             MainButtonShow(candle_price[j] * (-1));
         }
     });
-    candle_img[j].addEventListener('click', function (){
+    candle_img[j].addEventListener('click', function () {
         blurOpacity(this, candle_disc[j])
     })
     candle_disc[j].addEventListener("click", function () {
         blurOpacity(candle_img[j], this)
     })
 }
-function MainButtonShow(cost){
+
+function MainButtonShow(cost) {
     candle_totalCost += cost;
     tg.MainButton.setText(`Купить! ${candle_totalCost} ₽`)
     if (tg.MainButton.isVisible !== true) {
--- flowerpot.js
+++ flowerpot.js
@@ -1,21 +1,19 @@
-let flowerpot_price = [500, 500, 500, 500, 500]
-let flowerpot_item = [0, 0, 0, 0, 0]
-let flowerpot_cost_item = [0, 0, 0, 0, 0]
-let flowerpot_totalCost = 0
-let flowerpot_plus_btn_id = ["flowerpots_btn1", "flowerpots_btn2", "flowerpots_btn3", "flowerpots_btn4", "flowerpots_btn5"]
-const flowerpot_min_btn_id = ["flowerpots_min_btn1", "flowerpots_min_btn2", "flowerpots_min_btn3", "flowerpots_min_btn4", "flowerpots_min_btn5"]
-const flowerpot_label_id = ["flowerpots_count1", "flowerpots_count2", "flowerpots_count3", "flowerpots_count4", "flowerpots_count5"]
+let [flowerpot_price, flowerpot_item, flowerpot_cost_item] = [
+    [500, 500, 500, 500, 500, 500],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]]
+const [flowerpot_plus_btn_id, flowerpot_min_btn_id, flowerpot_label_id] = [
+    ["flowerpots_btn1", "flowerpots_btn2", "flowerpots_btn3", "flowerpots_btn4", "flowerpots_btn5", "tray_btn1"],
+    ["flowerpots_min_btn1", "flowerpots_min_btn2", "flowerpots_min_btn3", "flowerpots_min_btn4", "flowerpots_min_btn5", "tray_min_btn1"],
+    ["flowerpots_count1", "flowerpots_count2", "flowerpots_count3", "flowerpots_count4", "flowerpots_count5", "tray_count1"]]
+const [flowerpot_label, flowerpot_plus_btn, flowerpot_min_btn] = [
+    flowerpot_label_id.map(id => document.getElementById(id)),
+    flowerpot_plus_btn_id.map(id => document.getElementById(id)),
+    flowerpot_min_btn_id.map(id => document.getElementById(id))]
 const flowerpot_count = flowerpot_label_id.length
-let flowerpot_label = []
-let flowerpot_plus_btn = []
-let flowerpot_min_btn = []
+let flowerpot_totalCost = 0
 
-for (let i = 0; i < flowerpot_count; i++) {
-    flowerpot_label.push(document.getElementById(flowerpot_label_id[i]))
-    flowerpot_plus_btn.push(document.getElementById(flowerpot_plus_btn_id[i]))
-    flowerpot_min_btn.push(document.getElementById(flowerpot_min_btn_id[i]))
-}
-for (let j = 0; j <flowerpot_count; j++){
+for (let j = 0; j < flowerpot_count; j++) {
     flowerpot_plus_btn[j].addEventListener("click", function () {
         flowerpot_item[j] += 1;
         flowerpot_cost_item[j] = flowerpot_price[j] * flowerpot_item[j];
@@ -36,7 +34,7 @@ for (let j = 0; j <flowerpot_count; j++){
 function MainButtonShow(cost) {
     flowerpot_totalCost += cost;
     tg.MainButton.setText(`Купить! ${flowerpot_totalCost} ₽`)
-    if (tg.MainButton.isVisible !== true) {
+    if (!tg.MainButton.isVisible) {
         tg.MainButton.show();
     }
     if (candle_totalCost === 0) {
